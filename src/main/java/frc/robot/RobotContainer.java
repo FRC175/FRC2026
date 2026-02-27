c47352f9a4267e9f964689e57540618f08d58fa4
Mike3 <mikeydc03@gmail.com>
2026-02-27 15:18:32 -0500

    some constant updates

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -39,7 +39,7 @@ public class RobotContainer {
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final XboxController m_driverController =
-      new XboxController(OperatorConstants.kDriverControllerPort);
+      new XboxController(OperatorConstants.driverControllerPort);
 
   private final Climb climb;
   private final Shooter shooter;
